cc51bd13007484d1e0f81947473461fbe1587cff
yasuno0327 <5315076@ed.tus.ac.jp>
2019-12-27 19:14:34 +0900

    Get bearer token

--- twitter/auth.go
+++ twitter/auth.go
@@ -1,6 +1,8 @@
 package twitter
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,11 +11,18 @@ type AuthConfig struct {
 	APISecret string
 }
 
+type AuthResponse struct {
+	TokenType   string `"json":"token_type"`
+	AccessToken string `"json":"access_token"`
+}
+
 func (config *AuthConfig) getBearerToken() (string, error) {
 	var apiKey = config.APIKey
 	var apiSecret = config.APISecret
 	var request *http.Request
 	var response *http.Response
+	var body []byte
+	var authRes AuthResponse
 	var authUrl = "https://api.twitter.com/oauth2/token?grant_type=client_credentials"
 	request, err := http.NewRequest("POST", authUrl, nil)
 	if err != nil {
@@ -25,4 +34,14 @@ func (config *AuthConfig) getBearerToken() (string, error) {
 	if err != nil {
 		return "", err
 	}
+	defer response.Body.Close()
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	err = json.Unmarshal(body, &authRes)
+	if err != nil {
+		return "", err
+	}
+	return authRes.AccessToken, nil
 }
