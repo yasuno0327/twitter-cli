a64cb87cab812b4bcbd420a8208ffc6ea6d3edc3
yasuno0327 <5315076@ed.tus.ac.jp>
2020-01-07 08:28:50 +0900

    Add test of GetBearerToken function

---
+++ twitter/auth_test.go
@@ -0,0 +1,19 @@
+package twitter
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetBearerTokenSuccess(t *testing.T) {
+	apiKey := os.Getenv("TWITTER_API_KEY")
+	apiSecret := os.Getenv("TWITTER_API_SECRET")
+	config := AuthConfig{APIKey: apiKey, APISecret: apiSecret}
+	token, err := config.getBearerToken()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(token) <= 0 {
+		t.Fatal("can not get token.")
+	}
+}
